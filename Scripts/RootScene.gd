08c9b6d61a90e45b8bc5da94d72731b849482fcd
Ozymandias1 <mrbaek86@gmail.com>
2024-09-06 01:30:09 +0900

    타이틀 화면

---
+++ Scripts/RootScene.gd
@@ -0,0 +1,12 @@
+# RootScene.gd
+extends Node
+
+#region Title
+# StartGame 버튼 클릭
+func _on_title_on_start_game_btn_pressed() -> void:
+	pass # Replace with function body.
+
+# Exit 버튼 클릭
+func _on_title_on_exit_btn_pressed() -> void:
+	get_tree().quit()
+#endregion
---
+++ Scripts/Title.gd
@@ -0,0 +1,13 @@
+# Title.gd
+extends Control
+
+signal on_start_game_btn_pressed
+signal on_exit_btn_pressed
+
+# Start Game 버튼 클릭 시그널
+func _on_btn_start_game_pressed():
+	on_start_game_btn_pressed.emit()
+
+# Exit 버튼 클릭 시그널
+func _on_btn_exit_pressed():
+	on_exit_btn_pressed.emit()
