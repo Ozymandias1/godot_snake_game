1e8e23afbf59663e39a1138ef943604d0be63d77
Ozymandias1 <mrbaek86@gmail.com>
2024-09-11 02:36:41 +0900

    게임레벨 스크립트 주석작업

--- Scripts/GameLevel.gd
+++ Scripts/GameLevel.gd
@@ -3,11 +3,13 @@ extends Node
 
 @onready var player_spawner: MultiplayerSpawner = $PlayerSpawner
 
+# 시작
 func _ready() -> void:
 	if multiplayer.is_server():
 		NetworkManager.on_player_connected.connect(self._on_player_connected)
 		self._on_player_connected(1, NetworkManager.my_player_data)
 
+# 플레이어 접속 처리
 func _on_player_connected(peer_id: int, player_data: Dictionary) -> void:
 	player_spawner.spawn({
 		"peer_id": peer_id,
