c44368ba89415a82a4ec843376472730a8fe8014
Ozymandias1 <mrbaek86@gmail.com>
2024-09-12 02:17:09 +0900

    몸체 생성 애니메이션

--- Scripts/Body.gd
+++ Scripts/Body.gd
@@ -8,6 +8,13 @@ var peer_id: int = -1
 func _enter_tree() -> void:
 	self.set_multiplayer_authority(peer_id)
 
+# 시작
+func _ready() -> void:
+	# 시작시 스케일 애니메이션 처리
+	self.scale = Vector2.ZERO
+	var tween = get_tree().create_tween()
+	tween.tween_property(self, "scale", Vector2.ONE, 0.1)
+	
 # 몸체 초기화
 func initialize(player_peer_id: int, fill_color: Color, outline_color: Color, location: Vector2) -> void:
 	self.peer_id = player_peer_id
