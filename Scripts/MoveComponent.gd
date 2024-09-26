af84a0d27c31c7cb5fc6b8223378936630362b30
Ozymandias1 <mrbaek86@gmail.com>
2024-09-27 03:08:19 +0900

    무시목록 업데이트, 부스트 제거

--- Scripts/MoveComponent.gd
+++ Scripts/MoveComponent.gd
@@ -6,16 +6,13 @@ class_name MoveComponent
 @export var target: Node2D
 
 var initial_move_speed: float = 0.0
-var boost_move_speed: float = 0.0
 
 # 시작
 func _ready() -> void:
 	self.initial_move_speed = player.move_speed
-	self.boost_move_speed = self.initial_move_speed * 1.5
 
 # 업데이트
 func _process(delta: float) -> void:
 	if is_multiplayer_authority():
-		player.move_speed = self.boost_move_speed if Input.is_action_pressed("Boost") else self.initial_move_speed
 		var front = target.global_transform.x
 		target.global_position += player.move_speed * front * delta
